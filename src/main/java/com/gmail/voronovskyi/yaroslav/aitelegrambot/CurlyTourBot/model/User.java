01e65fcd115d9d3d727c61607669811f5ffe1a33
yaroslav <yaroslav.voronovskyi@gmail.com>
2023-06-12 11:22:03 +0300

    Created model and added dependencies.

---
+++ src/main/java/com/gmail/voronovskyi/yaroslav/aitelegrambot/CurlyTourBot/model/User.java
@@ -0,0 +1,35 @@
+package com.gmail.voronovskyi.yaroslav.aitelegrambot.CurlyTourBot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long chatId;
+    private Integer stateId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String phoneNumber;
+    private String email;
+    private Boolean admin;
+    private Boolean notified;
+}
